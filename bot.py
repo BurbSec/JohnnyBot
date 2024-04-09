b025a3595a8efa1f08e0a6578678241bb62eac58
Johnny Xmas <johnny+github@johnnyxmas.net>
2024-04-08 21:44:22 -0400

    Added task logging and reduced role check timer
    Added task logging and reduced role check timer

--- bot.py
+++ bot.py
@@ -1,12 +1,16 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+
 import os
+import logging
+from logging.handlers import RotatingFileHandler
 import discord
 from discord.ext import commands, tasks
-
 TOKEN = os.environ.get('DISCORD_BOT_TOKEN')
-BAD_BOT_ROLE_NAME = 'bad bots'
+BAD_BOT_ROLE_NAME = 'bad bots'  # Changed ROLE_NAME to BAD_BOT_ROLE_NAME
 MODERATOR_ROLE_NAME = 'moderators'
-BOT_SCAN_DELAY_MINUTES = 1
+DELAY_MINUTES = 1
+LOG_FILE = '/var/log/johnnybot.log'
+LOG_MAX_SIZE = 5 * 1024 * 1024  # 5MB
 
 if not TOKEN:
     print('DISCORD_BOT_TOKEN environment variable not set. Exiting...')
@@ -14,38 +18,54 @@ if not TOKEN:
 
 intents = discord.Intents.default()
 intents.members = True
+intents.messages = True
 intents.message_content = True
+
+# Set up logging
+logger = logging.getLogger('discord')
+logger.setLevel(logging.INFO)
+handler = RotatingFileHandler(LOG_FILE, maxBytes=LOG_MAX_SIZE, backupCount=5)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+handler.setFormatter(formatter)
+logger.addHandler(handler)
 
 bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_ready():
-    print(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
+    logger.info(f'Logged in as {bot.user.name} (ID: {bot.user.id})')
     update_bad_bots.start()
 
-#Mark users without roles as bad bots. If a user adds a role, remove the bad bot role:
 @tasks.loop(minutes=1)
 async def update_bad_bots():
-    for guild in bot.guilds:
-        bad_bots_role = discord.utils.get(guild.roles, name=BAD_BOT_ROLE_NAME)
-        if bad_bots_role:
-            for member in guild.members:
-                if not member.bot and bad_bots_role in member.roles:
-                    if len(member.roles) > 2:  # User has more than just @everyone and bad bots role
-                        await member.remove_roles(bad_bots_role, reason='User has been assigned additional roles')
-                        print(f'Removed {BAD_BOT_ROLE_NAME} role from {member.name} in {guild.name}')
-                    elif len(member.roles) == 1:  # Newly joined user with no assigned roles
-                        joined_at = member.joined_at
-                        delay = BOT_SCAN_DELAY_MINUTES  * 60
-                        if (discord.utils.utcnow() - joined_at).total_seconds() > delay:
-                            await member.add_roles(bad_bots_role, reason=f'No role assigned after {BOT_SCAN_DELAY_MINUTES} minutes')
-                            print(f'Assigned {BAD_BOT_ROLE_NAME} role to {member.name} in {guild.name}')
+    roles_modified = False
+    try:
+        for guild in bot.guilds:
+            bad_bots_role = discord.utils.get(guild.roles, name=BAD_BOT_ROLE_NAME)
+            if bad_bots_role:
+                for member in guild.members:
+                    if not member.bot and bad_bots_role in member.roles:
+                        if len(member.roles) > 2:
+                            await member.remove_roles(bad_bots_role, reason='User has been assigned additional roles')
+                            logger.info(f'Removed {BAD_BOT_ROLE_NAME} role from {member.name} in {guild.name}')
+                            roles_modified = True
+                        elif len(member.roles) == 1:
+                            joined_at = member.joined_at
+                            delay = DELAY_MINUTES * 60
+                            if (discord.utils.utcnow() - joined_at).total_seconds() > delay:
+                                await member.add_roles(bad_bots_role, reason=f'No role assigned after {DELAY_MINUTES} minutes')
+                                logger.info(f'Assigned {BAD_BOT_ROLE_NAME} role to {member.name} in {guild.name}')
+                                roles_modified = True
+    except Exception as e:
+        logger.error(f'Unable to complete task "update_bad_bots": {e}')
+    else:
+        if not roles_modified:
+            logger.debug(f'Task "update_bad_bots" completed without modifying roles')
 
 @update_bad_bots.before_loop
 async def before_update_bad_bots():
     await bot.wait_until_ready()
 
-#Delete messages posted by bad bots:
 @bot.event
 async def on_message(message):
     if message.author.bot:
@@ -54,10 +74,8 @@ async def on_message(message):
     bad_bots_role = discord.utils.get(message.author.guild.roles, name=BAD_BOT_ROLE_NAME)
     if bad_bots_role in message.author.roles:
         await message.delete()
-        print(f'Deleted message from {message.author.name} in {message.guild.name}')
+        logger.info(f'Deleted message from {message.author.name} in {message.guild.name}: {message.content}')
 
-
-#Alow modertors to make the bot send messages to a channel:
 @bot.tree.command(name='post', description='Post a message in a channel')
 @commands.has_role(MODERATOR_ROLE_NAME)
 async def post_message(interaction: discord.Interaction, channel: discord.TextChannel, message: str):
@@ -69,7 +87,7 @@ async def post_message_error(interaction: discord.Interaction, error):
     if isinstance(error, commands.MissingRole):
         await interaction.response.send_message(f'You need the {MODERATOR_ROLE_NAME} role to use this command.', ephemeral=True)
     else:
-        print(f'Error occurred: {error}')
+        logger.error(f'Error occurred: {error}')
 
 if __name__ == '__main__':
     bot.run(TOKEN)
